64401424e70d20b93c03f4ace88cf648833c4943
Alán F. Muñoz <afer.mg@gmail.com>
2025-07-24 15:33:28 -0400

    [WIP]feat: use latest torch, py312; fails (invalid ordinal) on moby

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   inputs = {
-    #nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
-    nixpkgs.url = "github:NixOS/nixpkgs/efcb904a6c674d1d3717b06b89b54d65104d4ea7";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    # nixpkgs.url = "github:NixOS/nixpkgs/efcb904a6c674d1d3717b06b89b54d65104d4ea7";
     nixpkgs_master.url = "github:NixOS/nixpkgs/master";
     systems.url = "github:nix-systems/default";
     flake-utils.url = "github:numtide/flake-utils";
@@ -23,10 +23,10 @@
           system = system;
           config = {
             allowUnfree = true;
-            cudaSupport = true; 
+            cudaSupport = true;
           };
         };
-      libList = [
+        libList = [
           pkgs.stdenv.cc.cc
           pkgs.stdenv.cc
           pkgs.libGL
@@ -40,23 +40,8 @@
       in
       with pkgs;
       rec {
-        # py311 = (
-        #     pkgs.python312
-        # pkgs.python312.override {
-        # packageOverrides = _: super: {
-        #   scikit-learn = super.scikit-learn.overridePythonAttrs (old: rec {
-        #     version = "1.2.2";
-        #     # skip checks, as a few fail but they are irrelevant
-        #     doCheck = false;
-        #     src = super.fetchPypi {
-        #       pname = "scikit-learn";
-        #       inherit version;
-        #       hash = "sha256-hCmuow7CTnqMftij+mITrfOBSm776gnhbgoMceGho9c=";
-        #     };
-        #   });
-        #   };
-        # }
         packages = {
+          # xformers = pkgs.python312.pkgs.callPackage ./nix/xformers.nix { };
           dinov2 = pkgs.python312.pkgs.callPackage ./nix/dinov2.nix { };
         };
         devShells = {
@@ -84,19 +69,19 @@
                 unset SOURCE_DATE_EPOCH
               '';
               shellHook = ''
-                export CUDA_PATH=${pkgs.cudaPackages.cudatoolkit}
-                export LD_LIBRARY_PATH=${pkgs.cudaPackages.cudatoolkit}/lib:${pkgs.cudaPackages.cudnn}/lib:$LD_LIBRARY_PATH
-                export NVCC_APPEND_FLAGS="-Xcompiler -fno-PIC"
-                export TORCH_CUDA_ARCH_LIST="6.0;6.1;7.0;7.5;8.0;8.6"
-                export CUDA_NVCC_FLAGS="-O2 -Xcompiler -fno-PIC"
                 runHook venvShellHook
                 # Set PYTHONPATH to only include the Nix packages, excluding current directory
                 export PYTHONPATH=${python_with_pkgs}/${python_with_pkgs.sitePackages}
-                # Ensure current directory is not in Python path
-                export PYTHONDONTWRITEBYTECODE=1
               '';
             };
         };
       }
     );
 }
+# export CUDA_PATH=${pkgs.cudaPackages.cudatoolkit}
+# export LD_LIBRARY_PATH=${pkgs.cudaPackages.cudatoolkit}/lib:${pkgs.cudaPackages.cudnn}/lib:$LD_LIBRARY_PATH
+# export NVCC_APPEND_FLAGS="-Xcompiler -fno-PIC"
+# export TORCH_CUDA_ARCH_LIST="6.0;6.1;7.0;7.5;8.0;8.6"
+# export CUDA_NVCC_FLAGS="-O2 -Xcompiler -fno-PIC"
+# # Ensure current directory is not in Python path
+# export PYTHONDONTWRITEBYTECODE=1
