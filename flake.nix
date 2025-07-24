3797cbe65aac33fce9c162921306e59ff5e63b5f
Johan Fredin Haslum <jfredinh@broadinstitute.org>
2025-07-24 17:30:07 -0400

    working on oppy

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
     nixpkgs_master.url = "github:NixOS/nixpkgs/master";
     systems.url = "github:nix-systems/default";
     flake-utils.url = "github:numtide/flake-utils";
@@ -42,7 +42,7 @@
           devShells = {
             default = let
               python_with_pkgs = (
-                python3.withPackages (pp: [
+                python312.withPackages (pp: [
                   (inputs.nahual-flake.packages.${system}.nahual)
                   packages.dinov2
                 ])
@@ -61,8 +61,17 @@
                   unset SOURCE_DATE_EPOCH
                 '';
                 shellHook = ''
+                  export CUDA_PATH=${pkgs.cudaPackages.cudatoolkit}
+                  export LD_LIBRARY_PATH=${pkgs.cudaPackages.cudatoolkit}/lib:${pkgs.cudaPackages.cudnn}/lib:$LD_LIBRARY_PATH
+                  export NVCC_APPEND_FLAGS="-Xcompiler -fno-PIC"
+                  export TORCH_CUDA_ARCH_LIST="6.0;6.1;7.0;7.5;8.0;8.6"
+                  export CUDA_NVCC_FLAGS="-O2 -Xcompiler -fno-PIC"
                   runHook venvShellHook
-                  export PYTHONPATH=${python_with_pkgs}/${python_with_pkgs.sitePackages}:$PYTHONPATH
+                  # export PYTHONPATH=${python_with_pkgs}/${python_with_pkgs.sitePackages}:$PYTHONPATH
+                  # Set PYTHONPATH to only include the Nix packages, excluding current directory
+                  export PYTHONPATH=${python_with_pkgs}/${python_with_pkgs.sitePackages}
+                  # Ensure current directory is not in Python path
+                  export PYTHONDONTWRITEBYTECODE=1
                 '';
               };
           };
