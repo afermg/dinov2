3acd56c7a70447298d57bd5e5fde6398033d71d4
Alán F. Muñoz <afer.mg@gmail.com>
2025-08-22 17:35:36 -0400

    refactor(server): migrate to py313; use nahual's server functions

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     nixpkgs_master.url = "github:NixOS/nixpkgs/master";
     systems.url = "github:nix-systems/default";
     flake-utils.url = "github:numtide/flake-utils";
@@ -8,15 +8,17 @@
     nahual-flake.url = "github:afermg/nahual";
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    flake-utils,
-    systems,
-    ...
-  } @ inputs:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      Systems,
+      ...
+    }@inputs:
     flake-utils.lib.eachDefaultSystem (
-      system: let
+      system:
+      let
         pkgs = import nixpkgs {
           system = system;
           config = {
@@ -25,56 +27,47 @@
           };
         };
       in
-        with pkgs; rec {
-          formatter = pkgs.alejandra;
-          apps.default = {
-            type = "app";
-            program =
-              writeShellApplication {
-                name = "app";
-                text = ''
-                  python server.py "$1"
-                '';
-              }
-              + "/bin/app";
-          };
-          packages = pkgs.callPackage ./nix {};
-          devShells = {
-            default = let
+      with pkgs;
+      rec {
+        formatter = pkgs.alejandra;
+        packages = pkgs.callPackage ./nix { };
+        devShells = {
+          default =
+            let
               python_with_pkgs = (
-                python312.withPackages (pp: [
+                python3.withPackages (pp: [
                   (inputs.nahual-flake.packages.${system}.nahual)
                   packages.dinov2
                 ])
               );
             in
-              mkShell {
-                packages = [
-                  python_with_pkgs
-                ];
-                currentSystem = system;
-                venvDir = "./.venv";
-                postVenvCreation = ''
-                  unset SOURCE_DATE_EPOCH
-                '';
-                postShellHook = ''
-                  unset SOURCE_DATE_EPOCH
-                '';
-                shellHook = ''
-                  export CUDA_PATH=${pkgs.cudaPackages.cudatoolkit}
-                  export LD_LIBRARY_PATH=${pkgs.cudaPackages.cudatoolkit}/lib:${pkgs.cudaPackages.cudnn}/lib:$LD_LIBRARY_PATH
-                  export NVCC_APPEND_FLAGS="-Xcompiler -fno-PIC"
-                  export TORCH_CUDA_ARCH_LIST="6.0;6.1;7.0;7.5;8.0;8.6"
-                  export CUDA_NVCC_FLAGS="-O2 -Xcompiler -fno-PIC"
-                  runHook venvShellHook
-                  # export PYTHONPATH=${python_with_pkgs}/${python_with_pkgs.sitePackages}:$PYTHONPATH
-                  # Set PYTHONPATH to only include the Nix packages, excluding current directory
-                  export PYTHONPATH=${python_with_pkgs}/${python_with_pkgs.sitePackages}
-                  # Ensure current directory is not in Python path
-                  export PYTHONDONTWRITEBYTECODE=1
-                '';
-              };
-          };
-        }
+            mkShell {
+              packages = [
+                python_with_pkgs
+              ];
+              currentSystem = system;
+              venvDir = "./.venv";
+              postVenvCreation = ''
+                unset SOURCE_DATE_EPOCH
+              '';
+              postShellHook = ''
+                unset SOURCE_DATE_EPOCH
+              '';
+              shellHook = ''
+                export CUDA_PATH=${pkgs.cudaPackages.cudatoolkit}
+                export LD_LIBRARY_PATH=${pkgs.cudaPackages.cudatoolkit}/lib:${pkgs.cudaPackages.cudnn}/lib:$LD_LIBRARY_PATH
+                export NVCC_APPEND_FLAGS="-Xcompiler -fno-PIC"
+                export TORCH_CUDA_ARCH_LIST="6.0;6.1;7.0;7.5;8.0;8.6"
+                export CUDA_NVCC_FLAGS="-O2 -Xcompiler -fno-PIC"
+                runHook venvShellHook
+                # export PYTHONPATH=${python_with_pkgs}/${python_with_pkgs.sitePackages}:$PYTHONPATH
+                # Set PYTHONPATH to only include the Nix packages, excluding current directory
+                export PYTHONPATH=${python_with_pkgs}/${python_with_pkgs.sitePackages}
+                # Ensure current directory is not in Python path
+                export PYTHONDONTWRITEBYTECODE=1
+              '';
+            };
+        };
+      }
     );
 }
--- server.py
+++ server.py
@@ -5,19 +5,18 @@ This is the only reliable messaging pattern in the suite, as it automatically wi
 
 """
 
-import json
 import sys
-import time
+from functools import partial
+from typing import Callable
 
 import numpy
 import pynng
 import torch
 import trio
-from nahual.serial import deserialize_numpy, serialize_numpy
 
 PARAMETERS = {}
 
-# address = "ipc:///tmp/reqrep.ipc"
+# address = "ipc:///tmp/dinov2.ipc"
 address = sys.argv[1]
 
 
@@ -39,7 +38,9 @@ def setup(
         A dictionary containing the device information and configuration parameters.
     """
     device = "cuda" if torch.cuda.is_available() else "cpu"
-    processor = torch.hub.load(repo_or_dir, model).to(device)
+    loaded_model = processor = torch.hub.load(repo_or_dir, model).to(device)
+
+    processor = partial(process, processor=loaded_model)
 
     PARAMETERS["repo_or_dir"] = repo_or_dir
     PARAMETERS["model"] = model
@@ -48,71 +49,7 @@ def setup(
     return processor, info
 
 
-async def responder(sock, processor):
-    """Asynchronous responder function for handling model setup and data processing.
-
-        This function continuously listens for incoming messages via a socket. It handles two
-        modes: initializing a model based on received parameters and processing data using
-        an already loaded model.
-
-        Parameters
-        ----------
-            sock: pynng. (object): The socket object used for receiving and sending messages.
-
-        Returns
-        -------
-            None: This function does not return a value but sends responses via the socket.
-
-        Raises
-        ------
-            Exception: If an error occurs during message handling or processing.
-
-
-    Notes:
-        - The function uses JSON for message serialization.
-        - The 'setup' function is called to initialize the model.
-        - The 'process' function is used to compute results from input data.
-    """
-
-    while True:
-        if processor is None:
-            try:
-                msg = await sock.arecv_msg()
-                if len(msg.bytes) == 1:
-                    print("Exiting")
-                    break
-                content = msg.bytes.decode()
-                parameters = json.loads(content)
-                if "model" in parameters:  # Start
-                    print("NODE0: RECEIVED REQUEST")
-                    processor, info = setup(**parameters)
-                    info_str = f"Loaded model with parameters {info}"
-                    print(info_str)
-                    print("Sending model info back")
-                    await sock.asend(json.dumps(info).encode())
-                    print("Model loaded. Will wait for data.")
-
-            except Exception as e:
-                print(f"Waiting for parameters: {e}")
-                time.sleep(1)
-        else:
-            try:
-                # Receive data
-                msg = await sock.arecv_msg()
-                if len(msg.bytes) == 1:
-                    print("Exiting")
-                    break
-                img = deserialize_numpy(msg.bytes)
-                # Add data processing here
-                result = process(img, processor=processor)
-                result_np = result.cpu().detach().numpy()
-                await sock.asend(serialize_numpy(result_np))
-
-            except Exception as e:
-                print(f"Waiting for data: {e}")
-
-
-def process(img: numpy.ndarray, processor) -> dict:
+def process(img: numpy.ndarray, processor: Callable) -> dict:
     """Process an image and masks to generate a graph-based tracking representation.
 
     Parameters
@@ -148,11 +85,13 @@ async def main():
     -------
     None
     """
-    processor = None
+    from nahual_server import responder
+
     with pynng.Rep0(listen=address, recv_timeout=300) as sock:
         print(f"Server listening on {address}")
         async with trio.open_nursery() as nursery:
-            nursery.start_soon(responder, sock, processor)
+            responder_wsetup = partial(responder, setup=setup)
+            nursery.start_soon(responder_wsetup, sock)
 
 
 if __name__ == "__main__":
